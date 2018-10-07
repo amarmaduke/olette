6027ff22b6d3633f7f524e84863ed24a3cd1e535
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2018-10-07 03:04:23 -0500

    cave in to bad practice

--- Makefile
+++
@@ -1,6 +0,0 @@
-all:
-	cargo install wasm-pack 2> /dev/null
-	wasm-pack build
-	cd www && npm install
-	npm link pkg
-	cd www && npm run build
---
+++ www/dist/0.bootstrap.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! exports provided: load_net, reduce_net, update_net */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"load_net\", function() { return load_net; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"reduce_net\", function() { return reduce_net; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"update_net\", function() { return update_net; });\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module 'olette'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\n\nfunction load_net(term) { return !(function webpackMissingModule() { var e = new Error(\"Cannot find module 'olette'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(term); }\nfunction reduce_net(index, kind) { return !(function webpackMissingModule() { var e = new Error(\"Cannot find module 'olette'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(index, kind); }\nfunction update_net(json) { return !(function webpackMissingModule() { var e = new Error(\"Cannot find module 'olette'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(json); }\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+}]);
